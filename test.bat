15a41ae3afcbf8d277ade3eb10d8151fee061157
fentcaty <ilovethickcuteasianpusesy@gmail.com>
2025-07-11 15:29:46 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,6 +1,13 @@
 @echo off
-set WEBHOOK_URL=https://discord.com/api/webhooks/1393213796222238751/uIbJ0LyMKEAtnfdlC4P9VZf4OUkjAWnQ42Kqh2TM5SZDxNTe0EBVAPrPogxP3vG9rG53
+setlocal
 
-powershell -Command ^
-  "$payload = @{content='hi'} | ConvertTo-Json; ^
-   Invoke-RestMethod -Uri '%WEBHOOK_URL%' -Method Post -Body $payload -ContentType 'application/json'"
+:: Set your webhook URL
+set "WEBHOOK_URL=https://discord.com/api/webhooks/1393213796222238751/uIbJ0LyMKEAtnfdlC4P9VZf4OUkjAWnQ42Kqh2TM5SZDxNTe0EBVAPrPogxP3vG9rG53"
+
+:: Send webhook using embedded PowerShell
+powershell -NoProfile -Command ^
+    "$url='%WEBHOOK_URL%';" ^
+    "$body = @{ content = 'hi' } | ConvertTo-Json -Compress;" ^
+    "Invoke-RestMethod -Uri $url -Method Post -Body $body -ContentType 'application/json'"
+
+endlocal
